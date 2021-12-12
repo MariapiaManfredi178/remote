52aa543b70f5806338f30c5539bc42ffd7289fcb
MariapiaManfredi178 <92304162+MariapiaManfredi178@users.noreply.github.com>
2021-12-12 21:53:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "remocolab.py",
     version = "0.1",
     py_modules = ['remocolab'],
-    url = "https://github.com/demotomohiro/remocolab",
+    url = "https://github.com/MariapiaManfredi178/remote/blob/master/remocolab.py",
     author = "demotomohiro",
     install_requires = ["pyngrok"]
 )
